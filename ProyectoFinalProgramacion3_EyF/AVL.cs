fc88231a252580d7a62bca8363424c679252124e
Emilio Con <40868204+ebcn0412@users.noreply.github.com>
2021-06-11 14:23:28 -0600

    dibujarArbolCambios

---
+++ ProyectoFinalProgramacion3_EyF/AVL.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProyectoFinalProgramacion3_EyF
+{
+    class AVL
+    {
+        public int valor;
+        public AVL NodoIzquierdo;
+        public AVL NodoDerecho;
+        public AVL NodoPadre;
+        public int altura;
+        public Rectangle prueba;
+        private DibujarAVL arbol;
+
+        public AVL()
+        {
+
+        }
+        public DibujarAVL Arbol
+        {
+            get { return arbol; }
+            set { arbol = value; }
+        }
+
+        //Constructor
+        public AVL(int valorNuevo, AVL izquierdo, AVL derecho, AVL padre)
+        {
+            valor = valorNuevo;
+            NodoIzquierdo = izquierdo;
+            NodoDerecho = derecho;
+            NodoPadre = padre;
+            altura = 0;
+        }
+        //Propiedades
+
+
+
+        //Insertar un nuevo valor al AVL
+        public AVL Insertar(int valorNuevo, AVL Raiz)
+        {
+            if (Raiz == null)
+                Raiz = new AVL(valorNuevo, null, null, null);
+            else if (valorNuevo < Raiz.valor)
+            {
+                Raiz.NodoIzquierdo = Insertar(valorNuevo, Raiz.NodoIzquierdo);
+            }
+            else if (valorNuevo > Raiz.valor)
+            {
+                Raiz.NodoDerecho = Insertar(valorNuevo, Raiz.NodoDerecho);
+            }
+            else
+            {
+                MessageBox.Show("Valor existente en el arbol", "Error", MessageBoxButtons.OK);
+            }
+
+            //Rotaciones Simples o Dobles segun el caso
+            if (Alturas(Raiz.NodoIzquierdo) - Alturas(Raiz.NodoDerecho) == 2)
+            {
+                if (valorNuevo < Raiz.NodoIzquierdo.valor)
+                    Raiz = RotacionIzquierdaSimple(Raiz);
+                else
+                    Raiz = RotacionIzquierdaDoble(Raiz);
+            }
+            if (Alturas(Raiz.NodoDerecho) - Alturas(Raiz.NodoIzquierdo) == 2)
+            {
+                if (valorNuevo > Raiz.NodoDerecho.valor)
+                    Raiz = RotacionDerechaSimple(Raiz);
+                else
+                    Raiz = RotacionDerechaDoble(Raiz);
+            }
+            Raiz.altura = max(Alturas(Raiz.NodoIzquierdo), Alturas(Raiz.NodoDerecho)) + 1;
+            return Raiz;
+        }
+
+        //Funcion para obtener que rama es mayor
+        private static int max(int lhs, int rhs)
+        {
+            return lhs > rhs ? lhs : rhs;
+        }
+        private static int Alturas(AVL Raiz)
+        {
+            return Raiz == null ? -1 : Raiz.altura;
+        }
+
+        AVL nodoE, nodoP;
+        public AVL Elimnar(int valorEliminar, ref AVL Raiz)
+        {
+            if (Raiz != null)
+            {
+                if (valorEliminar < Raiz.valor)
+                {
+                    nodoE = Raiz;
+                    Elimnar(valorEliminar, ref Raiz.NodoIzquierdo);
+                }
+                else
+                {
+                    if (valorEliminar > Raiz.valor)
+                    {
+                        nodoE = Raiz;
+                        Elimnar(valorEliminar, ref Raiz.NodoDerecho);
+                    }
+                    else
+                    {
+                        //Posocionamiento sobre el elemento a eliminar
+                        AVL NodoEliminar = Raiz;
+                        if (NodoEliminar.NodoDerecho == null)
+                        {
+                            Raiz = NodoEliminar.NodoIzquierdo;
+                            if (Alturas(nodoE.NodoIzquierdo) - Alturas(nodoE.NodoDerecho) == 2)
+                            {
+                                if (valorEliminar < nodoE.valor)
+                                    nodoP = RotacionIzquierdaSimple(nodoE);
+                                else
+                                    nodoE = RotacionDerechaSimple(nodoE);
+                            }
+                            if (Alturas(nodoE.NodoDerecho) - Alturas(NodoIzquierdo) == 2)
+                            {
+                                if (valorEliminar > nodoE.NodoDerecho.valor)
+                                    nodoE = RotacionDerechaSimple(nodoE);
+                                else
+                                    nodoE = RotacionDerechaDoble(nodoE);
+                                nodoP = RotacionDerechaSimple(nodoE);
+                            }
+                        }
+                        else
+                        {
+                            if (NodoEliminar.NodoIzquierdo == null)
+                            {
+                                Raiz = NodoEliminar.NodoDerecho;
+                            }
+                            else
+                            {
+                                if (Alturas(Raiz.NodoIzquierdo) - Alturas(Raiz.NodoDerecho) > 0)
+                                {
+                                    AVL AuxiliarNodo = null;
+                                    AVL Auxiliar = Raiz.NodoIzquierdo;
+                                    bool Bandera = false;
+                                    while (Auxiliar.NodoDerecho != null)
+                                    {
+                                        AuxiliarNodo = Auxiliar;
+                                        Auxiliar = Auxiliar.NodoDerecho;
+                                        Bandera = true;
+                                    }
+                                    Raiz.valor = Auxiliar.valor;
+                                    NodoEliminar = Auxiliar;
+                                    if (Bandera == true)
+                                    {
+                                        AuxiliarNodo.NodoDerecho = Auxiliar.NodoIzquierdo;
+                                    }
+                                    else
+                                    {
+                                        Raiz.NodoIzquierdo = Auxiliar.NodoIzquierdo;
+                                    }
+                                    //Realiza las rotaciones simples o dobles segun el caso
+                                }
+                                else
+                                {
+                                    if (Alturas(Raiz.NodoDerecho) - Alturas(Raiz.NodoIzquierdo) > 0)
+                                    {
+                                        AVL AuxiliarNodo = null;
+                                        AVL Auxiliar = Raiz.NodoDerecho;
+                                        bool Bandera = false;
+                                        while (Auxiliar.NodoIzquierdo != null)
+                                        {
+                                            AuxiliarNodo = Auxiliar;
+                                            Auxiliar = Auxiliar.NodoIzquierdo;
+                                            Bandera = true;
+                                        }
+                                        Raiz.valor = Auxiliar.valor;
+                                        NodoEliminar = Auxiliar;
+                                        if (Bandera == true)
+                                        {
+                                            AuxiliarNodo.NodoIzquierdo = Auxiliar.NodoDerecho;
+                                        }
+                                        else
+                                        {
+                                            Raiz.NodoDerecho = Auxiliar.NodoDerecho;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (Alturas(Raiz.NodoDerecho) - Alturas(Raiz.NodoIzquierdo) > 0)
+                                        {
+                                            AVL AuxiliarNodo = null;
+                                            AVL Auxiliar = Raiz.NodoIzquierdo;
+                                            bool Bandera = false;
+                                            while (Auxiliar.NodoDerecho != null)
+                                            {
+                                                AuxiliarNodo = Auxiliar;
+                                                Auxiliar = Auxiliar.NodoDerecho;
+                                                Bandera = true;
+                                            }
+                                            Raiz.valor = Auxiliar.valor;
+                                            NodoEliminar = Auxiliar;
+                                            if (Bandera == true)
+                                            {
+                                                AuxiliarNodo.NodoDerecho = Auxiliar.NodoIzquierdo;
+                                            }
+                                            else
+                                            {
+                                                Raiz.NodoIzquierdo = Auxiliar.NodoIzquierdo;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nodo inexistente en el arbol", "Error", MessageBoxButtons.OK);
+            }
+            return nodoP;
+        }
+
+
+
+        //Rotacion izquierda simple
+        private static AVL RotacionIzquierdaSimple(AVL k2)
+        {
+            AVL k1 = k2.NodoIzquierdo;
+            k2.NodoIzquierdo = k1.NodoDerecho;
+            k1.NodoDerecho = k2;
+            k2.altura = max(Alturas(k2.NodoIzquierdo), Alturas(k2.NodoDerecho)) + 1;
+            k1.altura = max(Alturas(k1.NodoIzquierdo), k2.altura) + 1;
+            return k1;
+        }
+
+        //Rotacion derecha simple
+        private static AVL RotacionDerechaSimple(AVL k1)
+        {
+            AVL k2 = k1.NodoDerecho;
+            k1.NodoDerecho = k2.NodoIzquierdo;
+            k2.NodoIzquierdo = k1;
+            k1.altura = max(Alturas(k1.NodoIzquierdo), Alturas(k1.NodoDerecho)) + 1;
+            k2.altura = max(Alturas(k2.NodoDerecho), k1.altura) + 1;
+            return k2;
+        }
+
+        //Doble rotacion izquierda
+        private static AVL RotacionIzquierdaDoble(AVL k3)
+        {
+            k3.NodoIzquierdo = RotacionDerechaSimple(k3.NodoIzquierdo);
+            return RotacionIzquierdaSimple(k3);
+        }
+
+        //Doble rotacion derecha
+        private static AVL RotacionDerechaDoble(AVL k1)
+        {
+            k1.NodoDerecho = RotacionIzquierdaSimple(k1.NodoDerecho);
+            return RotacionDerechaSimple(k1);
+        }
+
+        public int getAltura(AVL nodoActual)
+        {
+            if (nodoActual == null)
+                return 0;
+            else
+                return 1 + Math.Max(getAltura(nodoActual.NodoIzquierdo), getAltura(nodoActual.NodoDerecho));
+        }
+
+        public void buscar(int valorBuscar, AVL Raiz)
+        {
+            if (Raiz != null)
+            {
+                if (valorBuscar < Raiz.valor)
+                {
+                    buscar(valorBuscar, Raiz.NodoIzquierdo);
+                }
+                else
+                {
+                    if (valor > Raiz.valor)
+                    {
+                        buscar(valorBuscar, Raiz.NodoDerecho);
+                    }
+                }
+            }
+            else { }
+            //MessageBox.Show("Valor no encontrado", "Error", MessageBoxButtons.OK);
+        }
+
+
+        /*++++++++++FUNCIONES PARA DIBUJAR EL ARBOL++++++++++*/
+
+        private const int Radio = 30;
+        private const int DistanciaH = 40;
+        private const int DistanciaV = 10;
+
+        private int CoordenadaX;
+        private int CoordenadaY;
+
+        //Encuentra la pocision en donde crearse el nodo
+        public void PocisionNodo(ref int xmin, int ymin)
+        {
+            int aux1, aux2;
+            CoordenadaY = (int)(ymin + Radio / 2);
+
+            //Obtiene la posicion del Sub-Arbol izquierdo
+            if (NodoIzquierdo != null)
+            {
+                NodoIzquierdo.PocisionNodo(ref xmin, ymin + Radio + DistanciaV);
+            }
+            if ((NodoIzquierdo != null) && (NodoDerecho != null))
+            {
+                xmin += DistanciaH;
+            }
+
+            //Si existe el nodo derecho e izquierdo deja un espacio entre ellos
+            if (NodoDerecho != null)
+            {
+                NodoDerecho.PocisionNodo(ref xmin, ymin + Radio + DistanciaV);
+            }
+
+            //Posicion de nodos derecho e izquiedo
+            if (NodoIzquierdo != null)
+            {
+                if (NodoDerecho != null)
+                {
+                    //centro entre los nodos
+                    CoordenadaX = (int)((NodoIzquierdo.CoordenadaX + NodoDerecho.CoordenadaX) / 2);
+                }
+                else
+                {
+                    //no hay nodo derecho centrar al nodo izquierdo
+                    aux1 = NodoIzquierdo.CoordenadaX;
+                    NodoIzquierdo.CoordenadaX = CoordenadaX - 40;
+                    CoordenadaX = aux1;
+                }
+            }
+            else if (NodoDerecho != null)
+            {
+                aux2 = NodoDerecho.CoordenadaX;
+                //no hay nodo izquierdo centrar al nodo derecho
+                NodoDerecho.CoordenadaX = CoordenadaX + 40;
+                CoordenadaX = aux2;
+            }
+            else
+            {
+                //Nodo hoja
+                CoordenadaX = (int)(xmin + Radio / 2);
+                xmin += Radio;
+            }
+        }
+
+        //Dibuja las ramas de los nodos izquierdo y derecho
+        public void DubujarRamas(Graphics grafo, Pen Lapiz)
+        {
+            if (NodoIzquierdo != null)
+            {
+                grafo.DrawLine(Lapiz, CoordenadaX, CoordenadaY, NodoIzquierdo.CoordenadaX, NodoIzquierdo.CoordenadaY);
+                NodoIzquierdo.DubujarRamas(grafo, Lapiz);
+            }
+            if (NodoDerecho != null)
+            {
+                grafo.DrawLine(Lapiz, CoordenadaX, CoordenadaY, NodoDerecho.CoordenadaX, NodoDerecho.CoordenadaY);
+                NodoDerecho.DubujarRamas(grafo, Lapiz);
+            }
+        }
+
+        //Dinuja el nodo en la posicion espeficicada
+        public void DibujarNodo(Graphics grafo, Font fuente, Brush Relleno, Brush RellenoFuente, Pen Lapiz, int dato, Brush encuentro)
+        {
+            //dibuja el contorno del nodo
+            Rectangle rect = new Rectangle(
+                (int)(CoordenadaX - Radio / 2),
+                (int)(CoordenadaY - Radio / 2),
+                Radio, Radio);
+
+            if (valor == dato)
+            {
+                grafo.FillEllipse(encuentro, rect);
+            }
+            else
+            {
+                grafo.FillEllipse(encuentro, rect);
+                grafo.FillEllipse(Relleno, rect);
+            }
+            grafo.DrawEllipse(Lapiz, rect);
+
+            //dibuja el valor del nodo
+            StringFormat formato = new StringFormat();
+
+            formato.Alignment = StringAlignment.Center;
+            formato.LineAlignment = StringAlignment.Center;
+            grafo.DrawString(valor.ToString(), fuente, Brushes.Black, CoordenadaX, CoordenadaY, formato);
+
+            //Dibuja los nodos hijos derechos e izquierdos
+            if (NodoIzquierdo != null)
+            {
+                NodoIzquierdo.DibujarNodo(grafo, fuente, Brushes.YellowGreen, RellenoFuente, Lapiz, dato, encuentro);
+            }
+            if (NodoDerecho != null)
+            {
+                NodoDerecho.DibujarNodo(grafo, fuente, Brushes.Yellow, RellenoFuente, Lapiz, dato, encuentro);
+            }
+        }
+
+        public void Colorear(Graphics grafo, Font fuente, Brush Relleno, Brush RellenoFuente, Pen Lapiz)
+        {
+            //Dibuja el contorno del nodo
+
+            Rectangle rect = new Rectangle(
+                (int)(CoordenadaX - Radio / 2),
+                (int)(CoordenadaY - Radio / 2),
+                Radio, Radio);
+            prueba = new Rectangle((int)(CoordenadaX - Radio / 2), (int)(CoordenadaY - Radio / 2), Radio, Radio);
+
+            //Dibuja el nombre
+            StringFormat formato = new StringFormat();
+            formato.Alignment = StringAlignment.Center;
+            formato.LineAlignment = StringAlignment.Center;
+
+            grafo.DrawEllipse(Lapiz, rect);
+            grafo.FillEllipse(Brushes.Gray, rect);
+            grafo.DrawString(valor.ToString(), fuente, Brushes.Black, CoordenadaX, CoordenadaY, formato);
+        }
+    }
+}
---
+++ ProyectoFinalProgramacion3_EyF/DibujarAVL.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProyectoFinalProgramacion3_EyF
+{
+    class DibujarAVL
+    {
+        public AVL Raiz;
+        public AVL aux;
+
+        //Constructor
+        public DibujarAVL()
+        {
+            aux = new AVL();
+        }
+        public DibujarAVL(AVL RaizNueva)
+        {
+            Raiz = RaizNueva;
+        }
+
+        //Agregar un nievo valor al arbol
+        public void insertar(int dato)
+        {
+            if (Raiz == null)
+                Raiz = new AVL(dato, null, null, null);
+            else
+                Raiz = Raiz.Insertar(dato, Raiz);
+
+        }
+        //Eliminar un valor del arbol
+        public void Eliminar(int dato)
+        {
+            if (Raiz == null)
+                Raiz = new AVL(dato, null, null, null);
+            else
+                Raiz.Elimnar(dato, ref Raiz);
+        }
+
+        private const int Radio = 30;
+        private const int DistanciaH = 40;
+        private const int Distanciav = 10;
+        private int CoordenadaX;
+        private int CoordenadaY;
+
+        public void PosicionNodoRecorrido(ref int xmin, ref int ymin)
+        {
+            CoordenadaY = (int)(ymin + Radio / 2);
+            CoordenadaX = (int)(xmin + Radio / 2);
+
+        }
+
+        public void color(Graphics grafo, Font fuente, Brush Relleno, Brush RellenoFuente, Pen Lapiz, AVL Raiz, bool post, bool inor, bool preor)
+        {
+            Brush entorno = Brushes.Red;
+            if (inor == true)
+            {
+                if (Raiz != null)
+                {
+                    color(grafo, fuente, Brushes.Red, RellenoFuente, Lapiz, Raiz.NodoIzquierdo, post, inor, preor);
+                    Raiz.Colorear(grafo, fuente, entorno, RellenoFuente, Lapiz);
+                    Thread.Sleep(500);
+                    Raiz.Colorear(grafo, fuente, Relleno, RellenoFuente, Lapiz);
+                    color(grafo, fuente, Relleno, RellenoFuente, Lapiz, Raiz.NodoDerecho, post, inor, preor);
+
+                }
+
+            }
+            else if (preor == true)
+            {
+                if (Raiz != null)
+                {
+                    Raiz.Colorear(grafo, fuente, Brushes.Yellow, Brushes.Red, Pens.Black);
+                    Thread.Sleep(500);
+                    Raiz.Colorear(grafo, fuente, Brushes.White, Brushes.Black, Pens.Black);
+                    color(grafo, fuente, Brushes.Red, RellenoFuente, Lapiz, Raiz.NodoIzquierdo, post, inor, preor);
+                    color(grafo, fuente, Relleno, RellenoFuente, Lapiz, Raiz.NodoDerecho, post, inor, preor);
+                }
+            }
+            else if (post == true)
+            {
+                if (Raiz != null)
+                {
+                    color(grafo, fuente, Relleno, RellenoFuente, Lapiz, Raiz.NodoIzquierdo, post, inor, preor);
+                    color(grafo, fuente, Relleno, RellenoFuente, Lapiz, Raiz.NodoDerecho, post, inor, preor);
+                    Raiz.Colorear(grafo, fuente, entorno, RellenoFuente, Lapiz);
+                    Thread.Sleep(500);
+                    Raiz.Colorear(grafo, fuente, entorno, RellenoFuente, Lapiz);
+                }
+            }
+        }
+
+        public void colorearB(Graphics grafo, Font fuente, Brush Relleno, Brush RellenoFuente, Pen Lapiz, AVL Raiz, int busqueda)
+        {
+            Brush entorno = Brushes.Red;
+            if (Raiz != null)
+            {
+                Raiz.Colorear(grafo, fuente, entorno, RellenoFuente, Lapiz);
+                if (busqueda < Raiz.valor)
+                {
+                    Thread.Sleep(500);
+                    Raiz.Colorear(grafo, fuente, entorno, Brushes.Red, Lapiz);
+                    colorearB(grafo, fuente, Relleno, RellenoFuente, Lapiz, Raiz.NodoIzquierdo, busqueda);
+                }
+                else
+                {
+                    if (busqueda > Raiz.valor)
+                    {
+                        Thread.Sleep(500);
+                        Raiz.Colorear(grafo, fuente, entorno, RellenoFuente, Lapiz);
+                        colorearB(grafo, fuente, Relleno, RellenoFuente, Lapiz, Raiz.NodoDerecho, busqueda);
+                    }
+                    else
+                    {
+                        Raiz.Colorear(grafo, fuente, entorno, RellenoFuente, Lapiz);
+                        Thread.Sleep(500);
+                    }
+                }
+            }
+        }
+
+
+        //Dibujar el arbol
+        public void DibujarArbol(Graphics grafo, Font fuente, Brush Relleno, Brush RellenoFuente, Pen Lapiz, int dato, Brush encuentro)
+        {
+            int x = 100;
+            int y = 75;
+            if (Raiz == null) return;
+
+            //Posicion de todos los nodos.
+
+            Raiz.PocisionNodo(ref x, y);
+
+            //Dibuyja los enlaces entre los nodos
+            Raiz.DubujarRamas(grafo, Lapiz);
+
+            //Dibujar todos los nodos
+            Raiz.DibujarNodo(grafo, fuente, Relleno, RellenoFuente, Lapiz, dato, encuentro);
+        }
+
+        public int x1 = 100;
+        public int y2 = 75;
+        public void restablecer_valores()
+        {
+            x1 = 100;
+            y2 = 75;
+        }
+        public void buscar(int x)
+        {
+            if (Raiz == null)
+                MessageBox.Show("Arbol AVL vacio", "Error", MessageBoxButtons.OK);
+            else
+                Raiz.buscar(x, Raiz);
+        }
+    }
+}
